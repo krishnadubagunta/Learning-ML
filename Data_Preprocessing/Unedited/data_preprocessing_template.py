abc5351380c1440b51bf6ab402c0f9dd01022fb3
Krishna Dubagunta <dubagunta.saikrishna@outlook.com>
2018-01-12 18:19:22 -0500

    Add unedited files and create template

--- Data_Preprocessing/Unedited/data_preprocessing_template.py
+++ Data_Preprocessing/Unedited/data_preprocessing_template.py
@@ -17,10 +17,26 @@ dataset = pd.read_csv('datasets/Data.csv')
 X = dataset.iloc[:, :-1].values
 Y = dataset.iloc[:,3].values
 
+# Handle Missing Data
+imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0)
+imputer = imputer.fit(X[:,1:3])
+X[:,1:3] = imputer.transform(X[:,1:3])
+
+
+#Categorical Data handling
+labelEncoder_X = LabelEncoder()
+X[:,0] = labelEncoder_X.fit_transform(X[:,0])
+
+oneHotEncoder = OneHotEncoder(categorical_features = [0])
+X = oneHotEncoder.fit_transform(X).toarray()
+
+labelEncoder_Y = LabelEncoder()
+Y = labelEncoder_Y.fit_transform(Y)
+
 # Splitting the dataset into the Training set and Test set
 X_train, X_test, Y_Train, Y_Test = train_test_split(X,Y, test_size = 0.2, random_state = 0)
 
 # Feature Scalings
-'''sc_X  = sc()
+sc_X  = sc()
 X_train = sc_X.fit_transform(X_train)
-X_test = sc_X.transform(X_test)'''
+X_test = sc_X.transform(X_test)
--- Data_Preprocessing/data_preprocessing_template.R
+++ Data_Preprocessing/data_preprocessing_template.R
@@ -1,19 +1,6 @@
 # Importing Datasets
 dataset = read.csv('datasets/Data.csv')
 
-#Handling Missing Data
-dataset$Age = ifelse(is.na(dataset$Age),
-ave(dataset$Age, FUN = function(x) mean(x, na.rm=TRUE)),
-dataset$Age)
-dataset$Salary = ifelse(is.na(dataset$Salary),
-ave(dataset$Salary, FUN = function(x) mean(x, na.rm=TRUE)),
-dataset$Salary)
-
-# Encoding Categorical Data
-dataset$Country = factor(dataset$Country, levels = c('France','Spain','Germany'),
-labels = c(1,2,3) )
-dataset$Purchased = factor(dataset$Purchased,levels = c('No','Yes'),labels = c(0,1))
-
 # Splitting the dataset into Train set and Test set
 # install.packages('caTools') to install any tools in R
 
@@ -22,6 +9,6 @@ split = sample.split(dataset$Purchased, SplitRatio = 0.8)
 train_set = subset(dataset, split == TRUE)
 test_set = subset(dataset, split == FALSE)
 
-# Feature Scaling
-train_set[,2:3] = scale(train_set[,2:3])
-test_set[,2:3] = scale(test_set[,2:3])
+# # Feature Scaling
+# train_set[,2:3] = scale(train_set[,2:3])
+# test_set[,2:3] = scale(test_set[,2:3])
